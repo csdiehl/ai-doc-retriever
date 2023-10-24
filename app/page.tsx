208f6f7e5ba0ea0018b96a16ef22cf39625531a9
Caleb Diehl <cdiehl@ap.org>
2023-10-24 09:03:07 -0700

    delete unneeded tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import { ChatWindow } from "@/components/ChatWindow";
 
-export default function Home() {
+export default function AgentsPage() {
   const InfoCard = (
     <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
       <h1 className="text-3xl md:text-4xl mb-4">
-        ▲ Next.js + LangChain.js 🦜🔗
+        ▲ Next.js + LangChain.js Retrieval Chain 🦜🔗
       </h1>
       <ul>
-        <li className="text-l">
-          🤝
+        <li className="hidden text-l md:block">
+          🔗
           <span className="ml-2">
-            This template showcases a simple chatbot using{" "}
+            This template showcases how to perform retrieval with a{" "}
             <a href="https://js.langchain.com/" target="_blank">
               LangChain.js
             </a>{" "}
-            and the Vercel{" "}
+            chain and the Vercel{" "}
             <a href="https://sdk.vercel.ai/docs" target="_blank">
               AI SDK
             </a>{" "}
@@ -26,23 +26,45 @@ export default function Home() {
           </span>
         </li>
         <li className="hidden text-l md:block">
+          🪜
+          <span className="ml-2">The chain works in two steps:</span>
+          <ul>
+            <li className="ml-4">
+              1️⃣
+              <span className="ml-2">
+                First, it rephrases the input question into a
+                &quot;standalone&quot; question, dereferencing pronouns based on
+                the chat history.
+              </span>
+            </li>
+            <li className="ml-4">
+              2️⃣
+              <span className="ml-2">
+                Then, it queries the retriever for documents similar to the
+                dereferenced question and composes an answer.
+              </span>
+            </li>
+          </ul>
+        </li>
+        <li className="hidden text-l md:block">
           💻
           <span className="ml-2">
             You can find the prompt and model logic for this use-case in{" "}
-            <code>app/api/chat/route.ts</code>.
+            <code>app/api/chat/retrieval/route.ts</code>.
           </span>
         </li>
         <li>
-          🏴‍☠️
+          🐶
           <span className="ml-2">
-            By default, the bot is pretending to be a pirate, but you can change
-            the prompt to whatever you want!
+            By default, the agent is pretending to be a talking puppy, but you
+            can change the prompt to whatever you want!
           </span>
         </li>
-        <li className="hidden text-l md:block">
+        <li className="text-l">
           🎨
           <span className="ml-2">
-            The main frontend logic is found in <code>app/page.tsx</code>.
+            The main frontend logic is found in{" "}
+            <code>app/retrieval/page.tsx</code>.
           </span>
         </li>
         <li className="text-l">
@@ -59,10 +81,18 @@ export default function Home() {
             !
           </span>
         </li>
+        <li className="hidden text-l md:block">
+          🔱
+          <span className="ml-2">
+            Before running this example on your own, you&apos;ll first need to
+            set up a Supabase vector store. See the README for more details.
+          </span>
+        </li>
         <li className="text-l">
           👇
           <span className="ml-2">
-            Try asking e.g. <code>What is it like to be a pirate?</code> below!
+            Upload some text, then try asking e.g.{" "}
+            <code>What is a document loader?</code> below!
           </span>
         </li>
       </ul>
@@ -70,11 +100,14 @@ export default function Home() {
   );
   return (
     <ChatWindow
-      endpoint="api/chat"
-      emoji="🏴‍☠️"
-      titleText="Patchy the Chatty Pirate"
-      placeholder="I'm an LLM pretending to be a pirate! Ask me about the pirate life!"
+      endpoint="api/chat/retrieval"
       emptyStateComponent={InfoCard}
+      showIngestForm={true}
+      placeholder={
+        'I\'ve got a nose for finding the right documents! Ask, "What is a document loader?"'
+      }
+      emoji="🐶"
+      titleText="Dana the Document-Retrieving Dog"
     ></ChatWindow>
   );
 }
